ced8c8131850e7d1fc5907c964bb08eb06ea0b2c
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-06 17:39:11 +0500

    Добавлен список констант

---
+++ src/com/stegnin/proxy/util/Constants.java
@@ -0,0 +1,19 @@
+package com.stegnin.proxy.util;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class Constants {
+
+    public static Integer DEFAULT_API_PORT = 8080;
+
+    public static Integer DEFAULT_PROXY_PORT = 8989;
+
+    public static String DEFAULT_HOST_NAME = "localhost";
+
+    public static Integer DEFAULT_MAX_CONNECTIONS = 100;
+
+    public static Integer DEFAULT_TIMEOUT = 1_000 * 60 * 5; // 5 min
+
+}
