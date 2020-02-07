32d7ddc3bae64d3573d65509e146c9bbb7d33a30
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-07 14:24:16 +0500

    Переносим системные сообщения в константы.

--- src/com/stegnin/proxy/util/Constants.java
+++ src/com/stegnin/proxy/util/Constants.java
@@ -1,19 +1,29 @@
 package com.stegnin.proxy.util;
 
+import java.io.File;
+
 /**
  * @author Alexandr Stegnin
  */
 
 public class Constants {
 
-    public static Integer DEFAULT_API_PORT = 8080;
+    public static final Integer DEFAULT_API_PORT = 8080;
 
-    public static Integer DEFAULT_PROXY_PORT = 8989;
+    public static final Integer DEFAULT_PROXY_PORT = 8989;
 
-    public static String DEFAULT_HOST_NAME = "localhost";
+    public static final String DEFAULT_HOST_NAME = "localhost";
 
-    public static Integer DEFAULT_MAX_CONNECTIONS = 100;
+    public static final Integer DEFAULT_MAX_CONNECTIONS = 100;
 
-    public static Integer DEFAULT_TIMEOUT = 1_000 * 60 * 5; // 5 min
+    public static final Integer DEFAULT_TIMEOUT = 1_000 * 60 * 5; // 5 min
 
+    public static final String WRONG_ARGUMENTS_EXCEPTION = "Некорректные входные параметры! Проверьте правильность ввода:\n"
+            .concat("<host> - строка\n<api port> - число\n<proxy port> - число\n<max connections> - число");
+
+    public static final String WRITE_LOG_FILE_SUCCESS = "Сообщение успешно записано в лог файл.\n";
+
+    public static final String WRITE_LOG_FILE_FAILED = "При записи сообщения в лог файл возникла ошибка:\n";
+
+    public static final String PATH_TO_LOG_FILE = "..".concat(File.separator).concat("proxyLog.log");
 }
